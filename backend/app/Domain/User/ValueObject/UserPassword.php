ba93c31f442dc2fa9bbf46d8a11c69e6e5a5b844
nonz250 <hosaka.non.work@gmail.com>
2022-03-13 03:19:00 +0900

    fixed user password value.

---
+++ backend/app/Domain/User/ValueObject/UserPassword.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace Nonz250\Storage\App\Domain\User\ValueObject;
+
+use InvalidArgumentException;
+use Nonz250\Storage\App\Foundation\ValueObject\StringValue;
+
+final class UserPassword extends StringValue
+{
+    public const LENGTH = 64;
+
+    public function __construct(string $value)
+    {
+        $this->validate($value);
+        $this->value = $value;
+    }
+
+    protected function validate(string $value): void
+    {
+        if ($value === '') {
+            throw new InvalidArgumentException(sprintf('%s is required.', __CLASS__));
+        }
+        if (!ctype_print($value)) {
+            throw new InvalidArgumentException(sprintf('%s must be hex character only.', __CLASS__));
+        }
+        if (mb_strlen($value) !== self::LENGTH) {
+            throw new InvalidArgumentException(
+                sprintf('%s must be %s chars.', __CLASS__, self::LENGTH)
+            );
+        }
+    }
+}

53b084e19d2c699d3d9fa192a19748f93c55c101
nonz250 <hosaka.non.work@gmail.com>
2022-03-13 04:01:01 +0900

    added app name value.

---
+++ backend/tests/Domain/Client/ValueObject/AppNameTest.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Domain\Client\ValueObject;
+
+use InvalidArgumentException;
+use Nonz250\Storage\App\Domain\Client\ValueObject\AppName;
+use Nonz250\Storage\App\Foundation\ValueObject\StringValue;
+use PHPUnit\Framework\TestCase;
+use Tests\StringTestHelper;
+
+class AppNameTest extends TestCase
+{
+    public function test__construct(): AppName
+    {
+        $expected = StringTestHelper::randomByMb4(AppName::MAX_LENGTH);
+        $appName = new AppName($expected);
+        $this->assertInstanceOf(StringValue::class, $appName);
+        $this->assertSame($expected, (string)$appName);
+        return $appName;
+    }
+
+    public function testRequiredException(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $expected = '';
+        new AppName($expected);
+    }
+
+    public function testMaxLengthException(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $expected = StringTestHelper::randomByMb4(AppName::MAX_LENGTH + 1);
+        new AppName($expected);
+    }
+}

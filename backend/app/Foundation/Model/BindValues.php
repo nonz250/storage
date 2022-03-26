e2ab9f96a1edbbac7157fb8b7738b26c8df086fe
nonz250 <hosaka.non.work@gmail.com>
2022-03-27 04:02:30 +0900

    added bind values.

---
+++ backend/app/Foundation/Model/BindValues.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace Nonz250\Storage\App\Foundation\Model;
+
+use ArrayIterator;
+use InvalidArgumentException;
+use IteratorAggregate;
+
+final class BindValues implements IteratorAggregate
+{
+    private array $values = [];
+
+    public function bindValue(string $key, $value): self
+    {
+        $this->validate($key);
+        $this->values[$key] = $value;
+        return $this;
+    }
+
+    private function validate(string $key): void
+    {
+        if ($key === '') {
+            throw new InvalidArgumentException('Key is required.');
+        }
+        if (array_key_exists($key, $this->values)) {
+            throw new InvalidArgumentException('Key already exists.');
+        }
+    }
+
+    public function toArray(): array
+    {
+        return $this->values;
+    }
+
+    public function getIterator(): ArrayIterator
+    {
+        return new ArrayIterator($this->values);
+    }
+}
---
+++ backend/tests/Foundation/Model/BindValuesTest.php
@@ -0,0 +1,59 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Foundation\Model;
+
+use InvalidArgumentException;
+use Nonz250\Storage\App\Foundation\Model\BindValues;
+use PHPUnit\Framework\TestCase;
+use Tests\StringTestHelper;
+
+class BindValuesTest extends TestCase
+{
+    public function test__construct(): BindValues
+    {
+        $bindValues = new BindValues();
+
+        $expectedKey1 = StringTestHelper::random();
+        $expectedValue1 = StringTestHelper::random();
+        $bindValues->bindValue($expectedKey1, $expectedValue1);
+
+        $expectedKey2 = StringTestHelper::random();
+        $expectedValue2 = mt_rand();
+        $bindValues->bindValue($expectedKey2, $expectedValue2);
+
+        $expectedKey3 = StringTestHelper::random();
+        $expectedValue3 = mt_rand() % 2 === 1;
+        $bindValues->bindValue($expectedKey3, $expectedValue3);
+
+        $this->assertSame([
+            $expectedKey1 => $expectedValue1,
+            $expectedKey2 => $expectedValue2,
+            $expectedKey3 => $expectedValue3,
+        ], $bindValues->toArray());
+
+        return $bindValues;
+    }
+
+    public function testKeyRequiredException(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $bindValues = new BindValues();
+        $expectedKey1 = '';
+        $expectedValue1 = StringTestHelper::random();
+        $bindValues->bindValue($expectedKey1, $expectedValue1);
+    }
+
+    public function testOverlapException(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $bindValues = new BindValues();
+
+        $expectedKey1 = StringTestHelper::random();
+        $expectedValue1 = StringTestHelper::random();
+        $expectedValue2 = mt_rand();
+
+        $bindValues->bindValue($expectedKey1, $expectedValue1);
+        $bindValues->bindValue($expectedKey1, $expectedValue2);
+    }
+}

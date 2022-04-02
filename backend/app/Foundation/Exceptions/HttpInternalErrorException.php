ab070e9a50345b2000b64f5594dbea5e07c3b6c7
nonz250 <hosaka.non.work@gmail.com>
2022-04-02 20:54:56 +0900

    added internal server error exception.

---
+++ backend/app/Foundation/Exceptions/HttpInternalErrorException.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Nonz250\Storage\App\Foundation\Exceptions;
+
+use Fig\Http\Message\StatusCodeInterface;
+use Throwable;
+
+class HttpInternalErrorException extends HttpException
+{
+    public function __construct($description = '', $message = 'Internal Server Error.', $code = 0, Throwable $previous = null)
+    {
+        parent::__construct(StatusCodeInterface::STATUS_INTERNAL_SERVER_ERROR, $description, $message, $code, $previous);
+    }
+}
